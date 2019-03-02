540cbc62b14090a79ac7d1194a76e3439bcac750
princeivankent <princeivankentmtiburcio@gmail.com>
2019-03-02 12:51:12 +0800

    Bootstrap styling installed

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
-    <router-link to="/home" active-class="active">Home</router-link>
-    <router-link to="/about" active-class="active">About</router-link>
-    <router-link to="/contact" active-class="active">Contact</router-link>
-    <router-link to="/profile" active-class="active">Profile</router-link>
-    <router-view></router-view>
+    <NavBar />
+    <div class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
+
+<script>
+import NavBar from './NavBar'
+
+export default {
+  name: 'Main',
+  components: {NavBar}
+}
+</script>
---
+++ src/layouts/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" to="/home">
+          <img src="src/assets/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/home" active-class="active">Home <span class="sr-only">(current)</span></router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/about" active-class="active">About</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/contact" active-class="active">Contact</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/profile" active-class="active">Profile</router-link>
+            </li>
+          </ul>
+        </div>
+        <form class="form-inline">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+  </div>
+</template>
