c8da25b14486381d936d2a0a6dc90af38fc1d897
princeivankent <princeivankentmtiburcio@gmail.com>
2019-03-08 16:22:56 +0800

    Code Opt

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? `${process.env.VUE_APP_NAME}/public` : `/${process.env.VUE_APP_NAME}/`,
+  // Dynamic public path for dev/prod environment
+  publicPath: process.env.NODE_ENV === 'production' 
+    ? `${process.env.VUE_APP_NAME}/public` 
+    : `/${process.env.VUE_APP_NAME}/`,
 
-  // devServer: {
-  //   proxy: 'http://localhost:8080/basic'
-  // },
   // output built static files to Laravel's public dir.
   // note the "build" script in package.json needs to be modified as well.
   outputDir: '../public',
